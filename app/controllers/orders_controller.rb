31a2b9a2e5d5ff4992a6da9affea912279ad43b0
Jeremy Huot <logflume@live.com>
2020-08-19 21:02:56 +0000

    Add order details to order thank you page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,10 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @lineItems = []
+    @order.line_items.each do |item|
+      @lineItems.push(Product.where("id = #{item.product_id}"))
+    end
   end
 
   def create
