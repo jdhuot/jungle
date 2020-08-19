75663df655b815a52e7e0c9dda39b63acbf655c7
Jeremy Huot <logflume@live.com>
2020-08-19 21:47:54 +0000

    Add dashboard information to display product and category counts for the admin user with links to the admin/products page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['AUTH_LOGIN'], password: ENV['AUTH_PASSWORD']#, except: :index
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
