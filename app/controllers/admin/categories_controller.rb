49340984ccfa17dd5130c3a9a1f57bbcb48a8643
Jeremy Huot <logflume@live.com>
2020-08-20 16:14:35 +0000

    Update admin categories functionality

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['AUTH_LOGIN'], password: ENV['AUTH_PASSWORD']#, except: :index
 
   def index
     @categories = Category.all
