1d4fce77896ee0b0e6fb7a39eaed5695544ca0e2
Jeremy Huot <logflume@live.com>
2020-08-25 15:51:24 +0000

    Add test to ensure clicking a product on the home page opens product info page

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "ProductDetails", type: :feature, js: true do
+
 
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
 
-    10.times do |n|
+    1.times do |n|
       @category.products.create!(
-        name:  Faker::Hipster.sentence(3),
+        name:  'fakeTitle',
         description: Faker::Hipster.paragraph(4),
         image: open_asset('apparel1.jpg'),
         quantity: 10,
@@ -17,14 +18,12 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
+  scenario "They click on a product" do
     # ACT
     visit root_path
-
-    # DEBUG / VERIFY
-    # save_screenshot
-
-    expect(page).to have_css 'article.product', count: 10
+    click_on 'fakeTitle'
+    expect(page).to have_css 'article.product-detail'
   end
+
 
 end
